83e3c0b1ba787d1ce46c237095d031707f8e6ddb
faisalz23 <mubarokfaisal234@gmail.com>
2025-09-01 15:17:34 +0700

    update

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,13 +1,12 @@
-import { request } from "http";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (
-    request: NextRequest,
-    { params }: { params: { id: string } }
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) => {
-    const id = (await params).id;
-    return NextResponse.json(
-        { message: `fetch product ${id} ` },
-        { status: 200 }
-    );
+  const id = (await params).id;
+  return NextResponse.json(
+    { message: `fetch product ${id} ` },
+    { status: 200 }
+  );
 };
